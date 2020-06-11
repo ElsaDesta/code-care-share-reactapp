bc51b3576241456b724b8aea126caa9be54816cf
Elsa Desta <elsamenelik@gmail.com>
2020-06-12 00:01:00 +0200

    test heroku

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 if(process.env.NODE_ENV === "production") {
-  app.use('/', express.static(path.join(__dirname, "client/build")));
+  app.use('/', express.static(path.join(__dirname, "/client/build")));
 }
 app.use('/items', itemsRouter);
 
